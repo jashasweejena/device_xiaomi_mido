865c7fa5e569238c0ca9ebe60f28fa5fffacfa3c
Jashaswee Jena <j.ishujena2000@gmail.com>
2019-05-22 18:58:22 +0000

    Mido: Ignore neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 # Sepolicy
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
